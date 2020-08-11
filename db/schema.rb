a1e86a0f8f71129d62425c746b973652358c9c9f
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-08-11 09:39:58 -0300

    update user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_094821) do
+ActiveRecord::Schema.define(version: 2020_08_11_122135) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
   end
 
 end
