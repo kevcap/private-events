4de0cb852eb070209db5a2eae2b0649a6ee6e8f3
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-17 14:30:15 +0000

    Add my_events routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events
-  resources :users, only: [:new, :show, :create]
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  root 'users#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  root "events#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
+  get 'register_event', to: 'events#register_event'
+  get 'my_events', to: 'users#show'
 end
