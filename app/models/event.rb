ddcfb0cd9ff8ecc3366a18b28fbac32762ffa7a2
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-17 14:28:11 +0000

    Update the models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ApplicationRecord
+  belongs_to :creator, class_name: 'User'
+  has_many :invitations, foreign_key: :attended_event_id
+  has_many :attendees, through: :invitations
 
+  # upcoming events  and past event
+  scope :upcoming_events, -> { where('date >= ?', Time.zone.now) }
+  scope :previous_events, -> { where('date < ?', Time.zone.now) }
 end
