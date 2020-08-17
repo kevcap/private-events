54639565fc164e991bcb85081a046a5bb105ef92
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-17 15:19:14 +0000

    Reference the class name of the custom foreign keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: 'creator_id', class_name: 'Event'
-  has_many :invitations, foreign_key: :attendee_id
-  has_many :attended_events, through: :invitations
+  has_many :invitations, foreign_key: :attendee_id, class_name: 'Invitation'
+  has_many :attended_events, through: :invitations, source: 'Event'
 end
