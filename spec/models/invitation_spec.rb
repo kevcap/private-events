3371a6b3ed140e373eb7a4691665c768f2b52a8e
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-08-19 16:24:51 +0000

    Add unit test for Invitation model

---
+++ spec/models/invitation_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe Invitation, type: :model do
+	context 'ActiveRecord association' do
+		it 'belons to attendee' do
+			invitation = Invitation.reflect_on_association(:attendee)
+			expect(invitation.macro).to  eq(:belongs_to)
+		end
+
+		it 'belons to attendee' do
+			invitation = Invitation.reflect_on_association(:attended_event)
+			expect(invitation.macro).to  eq(:belongs_to)
+		end
+	end
+end
