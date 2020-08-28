92d7428bd14af1caaff8a5d37182a3633445578c
Kevin Alves <56762529+KevinAlvesJS@users.noreply.github.com>
2020-08-28 09:45:57 -0300

    Add remaining routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :events
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :users
+  resources :events, only: [ :new, :create, :show]
+  resources :sessions, only: [ :new, :create, :destroy]
+  resources :users, only: [ :new, :create, :show]
   root "events#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'login', to: 'sessions#new'
